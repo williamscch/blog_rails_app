3a605759bd51fa252c6812c7c9c6a489606e49a1
wdavidcch <david.13.2121@gmail.com>
2022-10-20 18:11:07 -0400

    Add models for the tables

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    user.increment!(:posts_counter)
+  end
+end
---
+++ db/migrate/20221020214215_rename_column_in_likes.rb
@@ -0,0 +1,5 @@
+class RenameColumnInLikes < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :likes, :posts_id, :post_id
+  end
+end
