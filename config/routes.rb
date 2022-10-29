860d1e03633803a7ed2d46875d31a23bc401c628
wdavidcch <david.13.2121@gmail.com>
2022-10-29 13:11:19 -0400

    Add forms to add a new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index new create show] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
   # Defines the root path route ("/")
   # root "articles#index"
