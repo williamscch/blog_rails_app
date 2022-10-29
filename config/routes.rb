5dd6045d8046f1ab6e1269b07ff66727963fa7f7
wdavidcch <david.13.2121@gmail.com>
2022-10-29 13:53:43 -0400

    Add forms for the comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show] do
       resources :comments, only: [:create]
