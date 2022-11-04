01bf599717518bea0a4d88e573b7176777dc07c2
wdavidcch <david.13.2121@gmail.com>
2022-11-04 16:56:57 -0400

    Add device setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,11 +46,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+
+  #devise settings 
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # smtp
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 587 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "users#index"
+  root to: 'users#index'
+  # get 'users/index'
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show] do
