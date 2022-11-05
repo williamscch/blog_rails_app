77bbb9f0d77235bab06e79ec1a3dd52a4413d0ff
wdavidcch <david.13.2121@gmail.com>
2022-11-05 02:02:45 -0400

    Add delete button for commits on the view and update the model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,4 @@ class CommentsController < ApplicationController
       render :new
     end
   end
-
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    redirect_to user_posts_path
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show destroy] do
-      resources :comments, only: [:create]
+      resources :comments, only: %i[create destroy]
       resources :likes, only: [:create]
     end
   end
